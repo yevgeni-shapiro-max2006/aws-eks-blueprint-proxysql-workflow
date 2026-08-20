f3793e1247d49bb9cb18436345eb38ff1be750ea
apps <23049337+yevgeni-shapiro-max2006@users.noreply.github.com>
2026-08-21 00:59:35 +0300

    update

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "terraform-app-workflow"
     key    = "eks-cluster-proxysql/terraform.tfstate"
-    region = "eu-west-1"
+    region = ""eu-central-1""
   }
 }
